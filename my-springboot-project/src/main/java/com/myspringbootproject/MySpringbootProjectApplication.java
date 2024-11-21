efbf5b0a7c644e23d455e2f5966fb1d38caeb00c
KesavaraRao <ch.kesavch@gmail.com>
2024-11-21 10:37:55 +0530

    commit the code.

---
+++ my-springboot-project/src/main/java/com/myspringbootproject/MySpringbootProjectApplication.java
@@ -0,0 +1,15 @@
+package com.myspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringbootProjectApplication.class, args);
+		
+		System.out.println("MySpringbootProjectApplication running in jenkin ..!.");
+	}
+
+}
